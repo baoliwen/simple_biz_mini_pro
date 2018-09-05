826b114a026d57ba3e5a664d34031ddce73c19e4
小包 <450541705@qq.com>
2018-09-06 00:19:24 +0800

    first commit

--- src/pages/address/address.js
+++ src/pages/address/address.js
@@ -16,7 +16,8 @@ Page({
     cityCode: '',
     districtCode: '',
     address: '',
-    openid:app.globalData.openid
+    id:'',
+    openid: app.globalData.openid
   },
   //事件处理函数
   bindViewTap: function () {
@@ -24,7 +25,7 @@ Page({
       url: '../logs/logs'
     })
   },
-  showModal(error) {
+  showToast(error) {
     wx.showToast({
       title: error.msg,
       icon: 'none',
@@ -73,7 +74,31 @@ Page({
     }, '请勾选1-2个敲码助手')
   },
   onLoad: function (options) {
-    console.log(options.id);
+    if (options.id) {
+      requestApi('address/wx/detail?addressId=' + options.id, 'GET', this.data).then(response => {
+        if (response.data.code === 200) {
+          this.setData({
+            customerName: response.data.data.customerName,
+            customerContact: response.data.data.customerContact,
+            provinceName: response.data.data.provinceName,
+            cityName: response.data.data.cityName,
+            districtName: response.data.data.districtName,
+            provinceCode: response.data.data.provinceCode,
+            cityCode: response.data.data.cityCode,
+            districtCode: response.data.data.districtCode,
+            address: response.data.data.address,
+            id:response.data.data.id
+          })
+        }
+      }, err => {
+        this.showToast({
+          msg: '查询信息失败',
+        })
+        wx.navigateTo({
+          url: '../index/index'
+        })
+      })
+    }
     this.initValidate();
   },
   bindRegionChange: function (e) {
@@ -93,26 +118,43 @@ Page({
       customerName: params.customerName,
       customerContact: params.customerContact,
       address: params.address,
-      openid:app.globalData.openid
+      openid: app.globalData.openid
     })
     // 传入表单数据，调用验证方法
     if (!this.WxValidate.checkForm(this.data)) {
       const error = this.WxValidate.errorList[0]
-      this.showModal(error)
-      
+      this.showToast(error)
+
       return false
     }
-    requestApi('address/wx/save', 'POST', this.data).then(response => {
-      if(response.data.code === 200){
-        wx.navigateTo({
-          url: '../index/index'
+
+
+    if(this.data.id){
+      requestApi('address/wx/update', 'POST', this.data).then(response => {
+        if (response.data.code === 200) {
+          wx.navigateTo({
+            url: '../index/index'
+          })
+        }
+      }, err => {
+        this.showToast({
+          msg: '保存失败',
         })
-      }
-    }, err => {
-      this.showModal({
-        msg: '保存失败',
+        console.log(err)
       })
-      console.log(err)
-    })
+    }else{
+      requestApi('address/wx/save', 'POST', this.data).then(response => {
+        if (response.data.code === 200) {
+          wx.navigateTo({
+            url: '../index/index'
+          })
+        }
+      }, err => {
+        this.showToast({
+          msg: '保存失败',
+        })
+        console.log(err)
+      })
+    }
   }
 })
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,16 +2,19 @@
 //获取应用实例
 const app = getApp()
 import { requestApi } from '../../utils/api'
+import touches from '../../utils/Touches'
 const constant = require('../../utils/util');
 Page({
   data: {
     keyword: '',
     records: [],
     pageNum: 1,
-    windowH:0,
+    windowH: 0,
     pageSize: constant.pageSize,
     totalPageNum: 0,
-    hasMoreData: true
+    hasMoreData: true,
+    startX: 0, //开始坐标
+    startY: 0
   },
   //事件处理函数
   bindViewTap: function () {
@@ -24,14 +27,14 @@ Page({
       keyword: e.detail.value
     })
   },
-  toAddAddress: function () {
+  toAddress: function () {
     wx.navigateTo({
-      url: '../addAddress/addAddress'
+      url: '../address/address'
     })
   },
-  addressDetail: function(e){
+  addressDetail: function (e) {
     wx.navigateTo({
-      url: '../addAddress/addAddress?id='+e.currentTarget.dataset.id
+      url: '../address/address?id=' + e.currentTarget.dataset.id
     })
   },
   onLoad: function () {
@@ -39,7 +42,7 @@ Page({
     wx.getSystemInfo({
       success: function (res) {
         that.setData({
-          windowH:res.windowHeight - 100
+          windowH: res.windowHeight - 100
         })
       }
     })
@@ -49,7 +52,7 @@ Page({
       that.initData();
     }
   },
-  onReachBottom:function(){
+  onReachBottom: function () {
     this.loadMore();
   },
   initData: function () {
@@ -64,6 +67,9 @@ Page({
       wx.hideLoading();
       let records = this.data.records;
       records = records.concat(response.data.data.records);
+      for (let i = 0; records.length > i; i++) {
+        records[i].isTouchMove = false; //默认全隐藏删除
+      }
       this.setData({
         totalPageNum: Math.ceil(response.data.data.total / pageSize),
         records: records
@@ -78,10 +84,73 @@ Page({
       console.log(err)
     });
   },
+  //手指触摸动作开始 记录起点X坐标
+  touchstart: function (e) {
+    //开始触摸时 重置所有删除
+    this.data.records.forEach(function (v, i) {
+      if (v.isTouchMove)//只操作为true的
+        v.isTouchMove = false;
+    })
+    this.setData({
+      startX: e.changedTouches[0].clientX,
+      startY: e.changedTouches[0].clientY,
+      records: this.data.records
+    })
+  },
+  //滑动事件处理
+  touchmove: function (e) {
+    var that = this,
+      index = e.currentTarget.dataset.index,//当前索引
+      startX = that.data.startX,//开始X坐标
+      startY = that.data.startY,//开始Y坐标
+      touchMoveX = e.changedTouches[0].clientX,//滑动变化坐标
+      touchMoveY = e.changedTouches[0].clientY,//滑动变化坐标
+      //获取滑动角度
+      angle = that.angle({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY });
+    that.data.records.forEach(function (v, i) {
+      v.isTouchMove = false
+      //滑动超过30度角 return
+      if (Math.abs(angle) > 30) return;
+      if (i == index) {
+        if (touchMoveX > startX) //右滑
+          v.isTouchMove = false
+        else //左滑
+          v.isTouchMove = true
+      }
+    })
+    //更新数据
+    that.setData({
+      records: that.data.records
+    })
+  },
+  /**
+   * 计算滑动角度
+   * @param {Object} start 起点坐标
+   * @param {Object} end 终点坐标
+   */
+  angle: function (start, end) {
+    var _X = end.X - start.X,
+      _Y = end.Y - start.Y
+    //返回角度 /Math.atan()返回数字的反正切值
+    return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
+  },
+  //删除事件
+  del: function (e) {
+    console.log(e);
+    let params = e.currentTarget.dataset;
+    requestApi('address/wx/delete?addressId='+params.id, 'DELETE', {}).then(response => {
+      this.data.records.splice(params.index, 1);
+      this.setData({
+        records: this.data.records
+      })
+    }, err => {
+      console.log(err)
+    })
+  },
   keywordSearch: function () {
-    this.setData({  
+    this.setData({
       pageNum: 1,   //第一次加载，设置1
-      records:[],  //放置返回数据的数组,设为空
+      records: [],  //放置返回数据的数组,设为空
     })
     this.initData();
   },
